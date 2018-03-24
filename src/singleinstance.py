df58aefc950eeb78d6ad12eca9007f5b969c4f5f
sigoa <si@goat.si>
2018-03-24 14:10:00 +0100

    i

--- src/singleinstance.py
+++ src/singleinstance.py
@@ -1,32 +1,30 @@
 #! /usr/bin/env python
 
 import atexit
+import errno
+from multiprocessing import Process
 import os
 import sys
 import state
 
 try:
     import fcntl  # @UnresolvedImport
-except ImportError:
+except:
     pass
-
 
 class singleinstance:
     """
-    Implements a single instance application by creating a lock file
-    at appdata.
+    Implements a single instance application by creating a lock file at appdata.
 
-    This is based upon the singleton class from tendo
-    https://github.com/pycontribs/tendo
-    which is under the Python Software Foundation License version 2
+    This is based upon the singleton class from tendo https://github.com/pycontribs/tendo
+    which is under the Python Software Foundation License version 2    
     """
     def __init__(self, flavor_id="", daemon=False):
         self.initialized = False
         self.counter = 0
         self.daemon = daemon
         self.lockPid = None
-        self.lockfile = os.path.normpath(
-            os.path.join(state.appdata, 'singleton%s.lock' % flavor_id))
+        self.lockfile = os.path.normpath(os.path.join(state.appdata, 'singleton%s.lock' % flavor_id))
 
         if not self.daemon and not state.curses:
             # Tells the already running (if any) application to get focus.
@@ -38,26 +36,19 @@ class singleinstance:
         self.initialized = True
         atexit.register(self.cleanup)
 
-    def lock(self, writePid = False):
+    def lock(self):
         if self.lockPid is None:
             self.lockPid = os.getpid()
         if sys.platform == 'win32':
             try:
-                # file already exists, we try to remove
-                # (in case previous execution was interrupted)
+                # file already exists, we try to remove (in case previous execution was interrupted)
                 if os.path.exists(self.lockfile):
                     os.unlink(self.lockfile)
-                self.fd = os.open(
-                    self.lockfile,
-                    os.O_CREAT | os.O_EXCL | os.O_RDWR | os.O_TRUNC
-                )
+                self.fd = os.open(self.lockfile, os.O_CREAT | os.O_EXCL | os.O_RDWR | os.O_TRUNC)
             except OSError:
                 type, e, tb = sys.exc_info()
                 if e.errno == 13:
-                    print(
-                        'Another instance of this application'
-                        ' is already running'
-                    )
+                    print 'Another instance of this application is already running'
                     sys.exit(-1)
                 print(e.errno)
                 raise
@@ -68,8 +59,7 @@ class singleinstance:
             self.fp = open(self.lockfile, 'a+')
             try:
                 if self.daemon and self.lockPid != os.getpid():
-                    # wait for parent to finish
-                    fcntl.lockf(self.fp, fcntl.LOCK_EX)
+                    fcntl.lockf(self.fp, fcntl.LOCK_EX) # wait for parent to finish
                 else:
                     fcntl.lockf(self.fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
                 self.lockPid = os.getpid()
@@ -78,10 +68,9 @@ class singleinstance:
                 sys.exit(-1)
             else:
                 pidLine = "%i\n" % self.lockPid
-                if writePid:
-                    self.fp.truncate(0)
-                    self.fp.write(pidLine)
-                    self.fp.flush()
+                self.fp.truncate(0)
+                self.fp.write(pidLine)
+                self.fp.flush()
 
     def cleanup(self):
         if not self.initialized:
@@ -108,5 +97,5 @@ class singleinstance:
                 fcntl.lockf(self.fp, fcntl.LOCK_UN)
                 if os.path.isfile(self.lockfile):
                     os.unlink(self.lockfile)
-        except Exception:
+        except Exception, e:
             pass
