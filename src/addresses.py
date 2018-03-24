a94b311b6c094d59660c68fbc56b48f98b1aac56
sigoa <si@goat.si>
2018-03-24 12:43:57 +0100

    4

--- src/addresses.py
+++ src/addresses.py
@@ -278,5 +278,4 @@ if __name__ == "__main__":
     print 'addressVersionNumber', addressVersionNumber
     print 'streamNumber', streamNumber
     print 'length of data(the ripe hash):', len(data)
-    print 'ripe data:', hexlify(data)
-
+    print 'ripe data:', hexlify(data) 
